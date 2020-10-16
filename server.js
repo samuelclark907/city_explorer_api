2f06cc9b96b20df783b2b9e6f8dff9aecfd91b4f
sam <samuelclark907@gmail.com>
2020-10-15 17:50:21 -0800

    initial setup

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const cors = require('cors');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
+
