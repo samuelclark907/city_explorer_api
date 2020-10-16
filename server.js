2f69016ee4909be0247124ba129d5f50b3fe9782
sam <samuelclark907@gmail.com>
2020-10-15 20:11:18 -0800

    deature 2 done

--- server.js
+++ server.js
@@ -12,7 +12,28 @@ const app = express();
 
 app.use(cors());
 
+// app.get('/', (request, response) => {
+//   response.send('Hello Sams World');
+// });
+
+app.get('/location', (request, response) => {
+  let city = request.query.city;
+  let data = require('./data/location.json')[0];
+  let location = new Location(data, city);
+  response.send(location);
+});
+
+app.get('weather', (request, response) => {
+
+});
+
 app.listen(PORT, () => {
   console.log(`running on ${PORT}`);
 });
 
+function Location(obj, query){
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+  this.search_query = query;
+  this.formatted_query = obj.display_name;
+}
